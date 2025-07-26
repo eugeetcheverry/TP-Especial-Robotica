94eea85c46497ee2a157395e8db739cef72aa2a6
Fabricio Della Vedova <108093588+fadellavedova@users.noreply.github.com>
2025-07-26 18:42:15 -0300

    oaaaa

--- simulador_matlab_hokuyo_2025a/initialize_particles.m
+++ simulador_matlab_hokuyo_2025a/initialize_particles.m
@@ -5,9 +5,8 @@ function particles = initialize_particles(count)
         normrnd(15.0, 2, 1, 1), ...
         unifrnd(-pi, pi, 1, 1)
     ];
-    i = 1;
+    i = 1
     while i < count
-        print('Hola estoy en el while')
         if map_is_free(particles(i, 1), particles(i, 2), [0, 0])
             i = i + 1
             aux = [
