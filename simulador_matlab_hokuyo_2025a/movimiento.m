fcde7c70ba3311bb08141b42d613d08e589ffa21
María Eugenia Etcheverry <114704607+eugeetcheverry@users.noreply.github.com>
2025-07-30 13:36:24 -0300

    Holi no se si esta bien

--- simulador_matlab_hokuyo_2025a/movimiento.m
+++ simulador_matlab_hokuyo_2025a/movimiento.m
@@ -1,13 +1,18 @@
-function movimiento(pose, particles):
+function cmd = movimiento(pose, particles):
     theta_robot = cumsum(particles(3, :))/size(particles(3,:));
     for i = [0:size(pose))]
         angle = atan2(pose(2, i+1) - pose(2, i), pose(1, i+1) - pose(1, i));
         if (theta_robot - 0.02*pi) < angle < (theta_robot + 0.03*pi)
             %Seguir como esty Vcmd igual wcmd cero
-            
-        elseif angle > theta_robot
-            %girar a la izquierda
-        elseif angle < theta_robot
-            %girar a la derecha
-    end
+            v_cmd = 0.5;
+            w_cmd = 0;
+        elseif angle > theta_robot %Girar a la izquied=rda
+            v_cmd = 0;
+            w_cmd = 0.35;
+        elseif angle < theta_robot %Girar a la derecha
+            v_cmd = 0;
+            w_cmd = -0.35;
+        end
+        cmd(i) = [v_cmd, w_cmd];
+    end 
 end
